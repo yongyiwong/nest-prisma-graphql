a9aea716a2df977b3c1debf1e01a304b257f09f6
Yong Yi Wong <yongyiwang.fullstack@gmail.com>
2024-10-09 09:55:57 -0700

    added unit test for UserService & UserResolver  for creatUser

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,19 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { PrismaService } from '../prisma/prisma.service';
-
+import { faker } from '@faker-js/faker';
 describe('UsersService', () => {
-  let service: UsersService;
+  let userService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UsersService, PrismaService],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    userService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  it('should create new user', async () => {
+    const fakeName = faker.person.firstName();
+    const fakeEmail = faker.internet.email();
+
+    const fakeUser = await userService.createUser(fakeName, fakeEmail);
+    expect(fakeUser).toBeDefined();
+    expect(fakeUser.name).toEqual(fakeName);
+    expect(fakeUser.email).toEqual(fakeEmail);
   });
 });
