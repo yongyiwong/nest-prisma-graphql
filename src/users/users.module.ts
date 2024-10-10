05f007cc23d6e0407b346b3d05bcb4b5ea8b6d7b
Yong Yi Wong <yongyiwang.fullstack@gmail.com>
2024-10-09 23:24:33 -0700

    fixed double forward ref

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,11 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { ProjectsModule } from '../projects/projects.module';
 
 @Module({
-  imports: [forwardRef(() => ProjectsModule)],
+  imports: [ProjectsModule],
   providers: [UsersResolver, UsersService, PrismaService],
   exports: [UsersService],
 })
